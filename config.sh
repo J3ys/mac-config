8a89555a8911969c47446f67c2ca4c49bd8ed31a
J3ys <jjjhaos@gmail.com>
2020-02-18 21:21:35 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,3 +1,5 @@
+#ESSENTIAL
+
 brew tap adoptopenjdk/openjdk
 brew cask install adoptopenjdk8
 
@@ -5,29 +7,34 @@ brew cask install intellij-idea
 brew install parallel
 brew install telnet
 brew install tcptraceroute
+brew cask install iterm2
+
+brew cask install google-chrome
+
+#TOOLS
 
 brew cask install sublime-text
 brew cask install postman
 brew cask install soapui
 brew cask install gitkraken
 brew cask install yourkit-java-profiler
+brew cask install xmind
 
-brew cask install google-chrome
+#COMMUNICATORS
+
 brew cask install microsoft-teams
 brew cask install microsoft-office
 brew cask install telegram
 brew cask install viber
 brew cask install skype
 brew cask install skype-for-business
+brew cask install slack
 
-brew cask install xmind
+#CONVENIENCE
+
 brew cask install evernote
 brew cask install spotify
 brew cask install tunnelbear
 brew cask install karabiner-elements
 brew cask install grammarly
 brew cask install lastpass
-brew cask install iterm2
-brew cask install slack
-
-
