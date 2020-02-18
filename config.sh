2d097b38661f402458a8429318af91b877e76e41
Viacheslav Sendel <viacheslav.sendel@lebara.com>
2020-02-18 09:22:27 +0200

    initial config

---
+++ config.sh
@@ -0,0 +1,29 @@
+xcode-select --install
+/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)”
+
+brew tap adoptopenjdk/openjdk
+brew cask install adoptopenjdk8
+
+brew install intellijidea
+brew install parallel
+
+brew cask install sublime-text
+brew cask install postman
+brew cask install soapui
+brew cask install gitkraken
+brew cask install yourkit-java-profiler
+
+brew cask install google-chrome
+brew cask install microsoft-teams
+brew cask install microsoft-office
+brew cask install telegram
+brew cask install viber
+brew cask install skype
+brew cask install skype-for-business
+
+brew cask install xmind
+brew cask install evernote
+brew cask install spotify
+brew cask install tunnelbear
+brew cask install karabiner-elements
+
