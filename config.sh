7d1bf5b7a351b28bc63e5323db9a2486e6a64aaf
J3ys <jjjhaos@gmail.com>
2020-02-18 09:56:42 +0200

    Update config.sh

--- config.sh
+++ config.sh
@@ -1,6 +1,3 @@
-#xcode-select --install
-/usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)" 
-
 brew tap adoptopenjdk/openjdk
 brew cask install adoptopenjdk8
 
