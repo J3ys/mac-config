9a0a55b26ac5d42dd05aa84fc0ccaeee24316ea7
J3ys <jjjhaos@gmail.com>
2020-04-09 16:42:55 +0300

    Update config.sh

--- config.sh
+++ config.sh
@@ -9,7 +9,8 @@ brew cask install intellij-idea
 brew install parallel
 brew install telnet
 brew install tcptraceroute
-brew cask install iterm2
+#oh my zsh
+sh -c "$(curl -fsSL https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 brew install htop
 brew install npm
 
